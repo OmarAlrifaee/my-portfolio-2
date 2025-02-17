48760f8bd30dcb342a39750629ff1daa3b3e02b6
OmarAlrifaee <jivara211@gmail.com>
2025-02-17 23:25:53 +0300

    add cv download name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,7 +38,7 @@ export const Home = () => {
         </p>
         <div className="flex items-center md:justify-normal justify-center gap-5 md:mt-8 mt-5">
           <Button variant={"destructive"} size={"lg"} className="font-bold">
-            <a href={"/cv.pdf"} download={true}>
+            <a href={"/cv.pdf"} download={"omar-alrifai"} >
               Download Cv
             </a>
           </Button>
